45781f5825fc54d46e82726241a391fc202fca9b
xxc <x007007007@hotmail.com>
2023-12-15 01:26:42 +0800

    update config

--- conf.d/007_ruby.bash
+++ conf.d/007_ruby.bash
@@ -1,4 +1,8 @@
 
+source ../../.bash/function/env.sh
+
+
 if [[ -e "$HOME/.rvm/bin" ]]; then
-    export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+#    export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+  __appendSearchNotExistPath "$HOME/.rvm/bin"
 fi
