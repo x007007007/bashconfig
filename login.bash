7bbcf7de01491d16ef6dcb8d549bf5fe785edb10
xxc <x007007007@hotmail.com>
2023-12-15 20:54:40 +0800

    update login loader

--- login.bash
+++ login.bash
@@ -3,13 +3,17 @@ echo load login initial ...
 install_path=$HOME/.bash
 platform=`uname |tr A-Z a-z`
 
-function smart_bash_load_login_config() {
-   for sub_platform in "" "/$platform"; do
-       for config_file_path in `find "${install_path}/login.d${sub_platform}" -name *.bash -type f -print`; do 
-           source "${config_file_path}"
-       done
-   done
+function __smart_bash_load_login_config() {
+  local script_path sub_platform config_file_path
+  for sub_platform in "" "/$platform" ; do
+    script_path=${install_path}/login.d${sub_platform}
+    if [[ -e "${script_path}" ]]; then
+      find "${script_path}" -name "*.bash" -type f -print |while read -r config_file_path; do
+        source "${config_file_path}"
+      done
+    fi
+  done
 }
 
-smart_bash_load_login_config
+__smart_bash_load_login_config
 
